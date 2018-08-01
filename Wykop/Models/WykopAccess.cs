a8af89c22babc4a41346bb62fa25733bd6a90b41
matihuf <matihuf@hotmail.com>
2018-08-01 22:33:29 +0200

    add json property

--- Wykop/Models/WykopAccess.cs
+++ Wykop/Models/WykopAccess.cs
@@ -1,9 +1,16 @@
-﻿namespace Wykop.Models
+﻿using Newtonsoft.Json;
+
+namespace Wykop.Models
 {
     public class WykopAccess
     {
+        [JsonProperty("ApiUrl")]
         public string ApiUrl { get; set; }
+
+        [JsonProperty("Key")]
         public string Key { get; set; }
+
+        [JsonProperty("Secret")]
         public string Secret { get; set; }
     }
 }
